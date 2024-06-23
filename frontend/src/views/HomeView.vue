a4b3b977be78a1cbc6e0efd07b0c61f7cecc810e
UnplugCharger <isaacbyron@gmail.com>
2024-06-23 12:25:27 +0300

    wip:front end code started

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import LoginUser from '@/components/LoginUser.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="green">Welcome To Qwetu</h1>
+    <LoginUser />
+  </main>
+</template>
